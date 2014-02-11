da65b5f4bae9b00ab4db0a945364e6f05977e112
Jasmine <jasmine.feng@rutgers.edu>
2014-02-10 23:07:46 -0500

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener{
+	
+	Button horsepic;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        horsepic = (Button) findViewById(R.id.button1);
+        horsepic.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		Intent click = new Intent(getBaseContext(), FuzzyEarHorse.class);
+		startActivity(click);
+	}
+    
+}
